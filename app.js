6eeccdd41b096f319d03a8993083a7663e00ba2b
Random <rc786986@gmail.com>
2025-06-05 14:16:42 +0530

    cors

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:5173',
             'http://localhost:3000',
+            'https://medical-student-app-frontend.onrender.com',
   ],
   credentials: true,
 }));
